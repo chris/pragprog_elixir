20b219b364d6462412dcddfdc94e1dce070bb16a
Christopher Bailey <chris@hoteltonight.com>
2013-12-07 12:45:15 -0800

    complete chapter 7 exercises (but not ideal)

--- lib/chapter7/lists.ex
+++ lib/chapter7/lists.ex
@@ -17,9 +17,66 @@ defmodule Chapter7.Lists do
   Adds n to each element of a character list, but wrapping if the addition
   results in a character greater than z.
   """
-  def caesar([], n), do: []
+  def caesar([], _n), do: []
   def caesar([head|tail], n) when n > 0, do: [charmap(head + n) | caesar(tail, n)]
 
   def charmap(char) when char > ?z, do: ?a + (char - ?z - 1)
   def charmap(char) when char <= ?z, do: char
+
+  @doc "Get a list of numbers in a inclusive range"
+  def span(from, to) when from > to, do: []
+  def span(from, to), do: [from | span(from+1,to)]
+
+  def all?([], _), do: true
+  def all?([head | tail], fun), do: fun.(head) and all?(tail, fun)
+
+  def each([], _fun), do: []
+  def each([head | tail], fun), do: [fun.(head) | each(tail, fun)]
+
+  def filter([], _fun), do: []
+  def filter([head | tail], fun) do
+    if fun.(head), do: [head | filter(tail, fun)], else: filter(tail, fun)
+  end
+
+  def take([], _), do: []
+  def take(_list, 0), do: []
+  def take(list, count) when count < 0, do: take(list, length(list) + count)
+  def take([head | tail], count) when count > 0, do: [head | take(tail, count-1)]
+
+  # TODO: this can't be the best way :(
+  def split([], _), do: { [], [] }
+  def split(list, 0), do: { [], list }
+  def split(list, count) when count < 0 and abs(count) > length(list), do: { [], list }
+  def split(list, count) when count < 0, do: split(list, length(list) + count)
+  def split(list, count) when count > 0 and count >= length(list), do: { list, [] }
+  def split(list, count) when count > 0, do: do_split(list, count, [])
+
+  def do_split([head|tail], count, acc) when length(acc) < count do
+    do_split(tail, count, acc ++ [head])
+  end
+  def do_split(list, count, acc) when length(acc) == count do
+    { acc, list }
+  end
+
+  def flatten([]), do: []
+  def flatten([head | tail]), do: flatten(head) ++ flatten(tail)
+  def flatten(x), do: [x]
+
+  def is_prime(num) do
+    factors = fn(num) -> span(2, trunc(:math.sqrt(num))) |> filter(&(&1 != num)) end
+    factors.(num) |> each(&(rem(num, &1))) |> filter(&(&1 == 0)) |> length == 0
+  end
+
+  def primes_up_to(n) when n > 1 do
+    # could also be done like this:
+    #lc num inlist span(2,n), factors.(num) |> each(&(rem(num, &1))) |> filter(&(&1 == 0)) |> length == 0, do: num
+    lc num inlist span(2,n), is_prime(num), do: num
+  end
+
+  def add_total_amount(orders, tax_rates) do
+    lc order inlist orders do
+      tax = 1 + Dict.get(tax_rates, order[:ship_to], 0.0)
+      Dict.put(order, :total_amount, order[:net_amount] * tax)
+    end
+  end
 end
--- test/chapter7_test.exs
+++ test/chapter7_test.exs
@@ -27,4 +27,78 @@ defmodule Chapter7Test do
   test "ListsAndRecursion-3" do
     assert caesar('ryvkve', 13) == 'elixir'
   end
+
+  test "ListsAndRecursion-4" do
+    assert span(7,10) == [7,8,9,10]
+    assert span(10,7) == []
+  end
+
+  test "ListsAndRecursion-5 all?" do
+    assert all?([1,2,3], &(&1 == 2)) == false
+    assert all?([2,6,20], &(rem(&1, 2) == 0)) == true
+  end
+
+  test "ListsAndRecursion-5 each" do
+    assert each([1,2,3], &(&1 * &1)) == [1,4,9]
+    assert each([], &(&1 * &1)) == []
+  end
+
+  test "ListsAndRecursion-5 filter" do
+    assert filter([1, 2, 9, 24], &(rem(&1, 2) == 0)) == [2, 24]
+    assert filter([0, true, false, 1, nil, "woah"], &(&1)) == [0, true, 1, "woah"]
+  end
+
+  test "ListsAndRecursion-5 split" do
+    assert split([1, 2, 3], 0)  == { [], [1,2, 3] }
+    assert split([1, 2, 3], -5) == { [], [1,2,3] }
+    assert split([1, 2, 3], 10) == { [1,2,3], [] }
+    assert split([1, 2, 3], 2)  == { [1,2], [3] }
+    assert split([1, 2, 3], -1) == { [1,2], [3] }
+    assert split([], 3)         == { [], [] }
+  end
+
+  test "ListsAndRecursion-5 take" do
+    assert take([], 7) == []
+    assert take([1, 2, 3], 0) == []
+    assert take([1, 2, 3], 2) == [1,2]
+    assert take([1, 2, 3], 10) == [1,2,3]
+    assert take([1, 2, 3], -1) == [1,2]
+  end
+
+  test "ListsAndRecursion-6 flatten" do
+    assert flatten([ 1, [ 2, 3, [4] ], 5, [[[ 6]]]]) == [1,2,3,4,5,6]
+    assert flatten([ [1,2], [ 3, [4] ], 5, [[[ 6]]]]) == [1,2,3,4,5,6]
+  end
+
+  test "ListsAndRecursion-7" do
+    assert primes_up_to(20) == [2,3,5,7,11,13,17,19]
+    assert primes_up_to(2) == [2]
+  end
+
+  test "ListsAndRecursion-8" do
+    tax_rates = [ NC: 0.075, TX: 0.08 ]
+    orders = [
+      [ id: 123, ship_to: :NC, net_amount: 100.00 ],
+      [ id: 124, ship_to: :OK, net_amount:  35.50 ],
+      [ id: 125, ship_to: :TX, net_amount:  24.00 ],
+      [ id: 126, ship_to: :TX, net_amount:  44.80 ],
+      [ id: 127, ship_to: :NC, net_amount:  25.00 ],
+      [ id: 128, ship_to: :MA, net_amount:  10.00 ],
+      [ id: 129, ship_to: :CA, net_amount: 102.00 ],
+      [ id: 120, ship_to: :NC, net_amount:  50.00 ]
+    ]
+    orders_with_tax = [
+      [ total_amount: 107.50,  id: 123, ship_to: :NC, net_amount: 100.00 ],
+      [ total_amount:  35.50,  id: 124, ship_to: :OK, net_amount:  35.50 ],
+      [ total_amount:  25.92,  id: 125, ship_to: :TX, net_amount:  24.00 ],
+      [ total_amount:  48.384, id: 126, ship_to: :TX, net_amount:  44.80 ],
+      [ total_amount: 26.875,  id: 127, ship_to: :NC, net_amount:  25.00 ],
+      [ total_amount:  10.00,  id: 128, ship_to: :MA, net_amount:  10.00 ],
+      [ total_amount: 102.00,  id: 129, ship_to: :CA, net_amount: 102.00 ],
+      [ total_amount:  53.75,  id: 120, ship_to: :NC, net_amount:  50.00 ]
+    ]
+
+    assert add_total_amount(orders, tax_rates) == orders_with_tax
+  end
+
 end
